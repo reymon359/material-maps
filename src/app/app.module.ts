a6329843876df31ef12f295cde3fcbe352bf7d89
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-07 12:26:08 +0200

    updated to angular 8.0.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 // Angular material 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
