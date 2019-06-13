d7cc0f1d5a2b043c5e79d62dca1605d6d79587af
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-13 07:56:59 +0200

    adding new markers

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from '../../classes/marker.class';
 
 @Component({
   selector: 'app-map',
@@ -6,11 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  lat = 51.678418;
-  lng = 7.809007;
-  constructor() { }
+
+  markers: Marker[] = [];
+
+  lat = 38.344612;
+  lng = -0.490443;
+  
+  constructor() {
+
+    const newMarker = new Marker(38.344612, -0.490443);
+    this.markers.push(newMarker);
+
+  }
 
   ngOnInit() {
+  }
+
+  addMarker(event) {
+
+    // Creating a coords object and predefining it
+    const coords: { lat: number, lng: number } = event.coords;
+
+    const newMarker = new Marker(coords.lat, coords.lng);
+
+    this.markers.push(newMarker);
+
   }
 
 }
