7a6107e1be32aa7dd6c232b98df08d17a5f9dd16
ramon morcillo <ramonmorcillocascales@gmail.com>
2019-06-13 08:16:45 +0200

    angular material snackbar

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Marker } from '../../classes/marker.class';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-map',
@@ -13,7 +14,7 @@ export class MapComponent implements OnInit {
   lat = 38.344612;
   lng = -0.490443;
 
-  constructor() {
+  constructor(private snackBar: MatSnackBar) {
 
     if (localStorage.getItem('markers')) {
       this.markers = JSON.parse(localStorage.getItem('markers'));
@@ -35,13 +36,18 @@ export class MapComponent implements OnInit {
 
     this.saveStorage();
 
+    this.snackBar.open('Marker added', 'Close', { duration: 3000 });
+
   }
 
-  removeMarker(i: number) {
+  deleteMarker(i: number) {
 
     this.markers.splice(i, 1);
 
     this.saveStorage();
+
+    this.snackBar.open('Marker deleted', 'Close', { duration: 3000 });
+
   }
 
 
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,6 +6,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import {MatCardModule} from '@angular/material/card';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+
 @NgModule({
   declarations: [],
   imports: [
@@ -13,13 +15,15 @@ import {MatCardModule} from '@angular/material/card';
     MatToolbarModule,
     MatButtonModule,
     MatProgressBarModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule
   ],
   exports: [
     MatToolbarModule,
     MatButtonModule,
     MatProgressBarModule,
-    MatCardModule
+    MatCardModule,
+    MatSnackBarModule
 
   ]
 })
